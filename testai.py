160326395a1af77ae4294145a93f0970324afd0c
Nicholas Osborn <nicholas.p.osborn@gmail.com>
2020-05-06 17:34:44 -0400

    still missing a logic piece

--- testai.py
+++ testai.py
@@ -6,18 +6,19 @@ with open('puzzles.txt', 'r') as f:
     solutions = data[1::2]
 
 
-for sol, puz in zip(solutions, puzzles):
+for sol, puz in zip(solutions[2:3], puzzles[2:3]):
     game = Sudoku(solution=sol, puzzle=puz)
     game.print_puzzle()
     ai = SudokuAI(game)
-    ai.solve()
-    # ai.axioms()
-    # for i in range(25):
-    #     print(f"step {i}")
-    #     print(game.progress())
-    #     ai.infer()
-    #     game.print_puzzle()
-    # for s in ai.knowledge:
-    #     if (8, 6) in s.boxes:
-    #         print(s)
+    # ai.solve()
+    ai.axioms()
+    for i in range(410):
+        print(f"step {i}")
+        print(game.progress())
+        ai.infer()
+        game.print_puzzle()
+    for s in ai.knowledge:
+        cells = {(8, 6), (8, 7)}
+        if cells.intersection(s.boxes):
+            print(s)
     print(game.grid_to_string(game.puzzle))
